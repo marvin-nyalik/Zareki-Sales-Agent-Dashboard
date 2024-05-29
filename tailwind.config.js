b64e7ff4c36c6b2b49947d6602b9ec3f474236d3
MARVIN <o.silverygold@gmail.com>
2024-05-29 11:22:09 +0300

    Update tailwind path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/components/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
