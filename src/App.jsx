3aa8041a0575f6647f7551fc6a15dddd290f0334
MARVIN <o.silverygold@gmail.com>
2024-06-01 21:02:15 +0300

    Lift state fetch up for optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,54 @@
-import React, { Suspense } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import store from "./redux/store";
+import { fetchCollections } from "./redux/collections/collectionSlice";
+import { fetchSchools } from "./redux/schools/schoolsSlice";
+import { fetchInvoices } from "./redux/invoices/invoiceSlice";
+import Dashboard from "./components/Dashboard";
+import Loading from "./components/Loading";
 
 const Schools = React.lazy(() => import("./components/Schools"));
 const SchoolDetails = React.lazy(() => import("./components/SchoolDetails"));
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCollections());
+    dispatch(fetchSchools());
+    dispatch(fetchInvoices());
+  }, [dispatch]);
+
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route
-            path="/schools"
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <Schools />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/schools/:name"
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <SchoolDetails />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/schools"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <Schools />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/schools/:name"
+          element={
+            <React.Suspense fallback={<Loading />}>
+              <SchoolDetails />
+            </React.Suspense>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default Root;
