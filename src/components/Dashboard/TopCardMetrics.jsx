9a4078a230d40b3e22818d9cdd4321f85854dd94
MARVIN <o.silverygold@gmail.com>
2024-06-02 11:04:09 +0300

    Add maxValue prop to cards

--- src/components/Dashboard/TopCardMetrics.jsx
+++ src/components/Dashboard/TopCardMetrics.jsx
@@ -28,20 +28,21 @@ const TopCardMetrics = ({ invoices, collections, schools }) => {
       <div className="mb-2 text-2xl text-blue-400">Top Metrics</div>
       <div className={isMobile ? styles["mobile-cont"] : styles.container}>
         {/* Collections card */}
-        <Card label={"Collections"} count={collections.length} />
+        <Card label={"Collections"} count={collections.length} maxValue={100} />
 
         {/* Sign Ups Card */}
-        <Card label={"Sign Ups"} count={totalSignUps} details={productCounts} />
+        <Card label={"Sign Ups"} count={totalSignUps} details={productCounts} maxValue={50} />
 
         {/* Revenue Card */}
         <Card
           label={"Revenue"}
           count={totalRevenue}
           details={revenuePerInvoiceItem}
+          maxValue={500000}
         />
 
         {/* Bounced Cheques card */}
-        <Card label={"Bounced Cheques"} count={bouncedCheques} />
+        <Card label={"Bounced Cheques"} count={bouncedCheques} maxValue={30}/>
       </div>
     </div>
   );
